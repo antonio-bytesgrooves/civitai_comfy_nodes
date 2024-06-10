cade3c2a9a536cf7b19718c5ccf39a41c537a6c6
antonio-bytesgrooves <168333329+antonio-bytesgrooves@users.noreply.github.com>
2024-06-11 01:00:24 +0200

    Update civitai_lora_loader.py

--- civitai_lora_loader.py
+++ civitai_lora_loader.py
@@ -44,7 +44,7 @@ class CivitAI_LORA_Loader:
 
             },
             "optional": {
-                "api_key": ("STRING", {"default": None, "multiline": False}),
+                "api_key": ("STRING", {"default": "503d493e9d6794d2ab3542db734f03b1", "multiline": False}),
                 "download_chunks": ("INT", {"default": 4, "min": 1, "max": 12, "step": 1}),
                 "download_path": (list(lora_paths),),
             },
